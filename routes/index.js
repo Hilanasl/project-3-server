f8c759d567439f546ece227b8b10a6f9d4f956f2
Hilana <hilana.sene@gmail.com>
2022-02-05 23:21:05 +0100

    details page added

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,34 @@
 const express = require("express");
 var router = express.Router();
 const protectRoute = require("../middlewares/protectRoute");
-const Trip = require('./../models/TripModel');
-const Day = require('./../models/DayModel');
-const User = require('./../models/UserModel');
-
-
+const Trip = require("./../models/TripModel");
+const Day = require("./../models/DayModel");
+const User = require("./../models/UserModel");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-router.get('/trips', (req, res, next) => {
-	Trip.find()
-		.populate('days')
-		.populate('author')
-		.then((dbRes) => {
-			res.status(200).json(dbRes);
-		})
-		.catch(next);
+router.get("/trips", (req, res, next) => {
+  Trip.find()
+    .populate("days")
+    .populate("author")
+    .then((dbRes) => {
+      res.status(200).json(dbRes);
+    })
+    .catch(next);
+});
+
+router.get("/trips/:id", (req, res, next) => {
+  Trip.findById(req.params.id)
+    .populate("days")
+    .populate("author")
+    .then((dbRes) => {
+      console.log("this is back:", dbRes);
+      res.status(200).json(dbRes);
+    })
+    .catch(next);
 });
 
 module.exports = router;
