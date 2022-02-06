12348186cf0113c19e10471952dd0d96ee89f50c
Anna Baldwin <annabaldwin56@gmail.com>
2022-02-06 14:16:08 +0100

    sunday changes

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const protectRoute = require("../middlewares/protectRoute");
 const Trip = require("./../models/TripModel");
 const Day = require("./../models/DayModel");
 const User = require("./../models/UserModel");
+const Activity = require("./../models/ActivityModel");
+
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -24,11 +26,22 @@ router.get("/trips/:id", (req, res, next) => {
   Trip.findById(req.params.id)
     .populate("days")
     .populate("author")
+    .populate('image')
     .then((dbRes) => {
       console.log("this is back:", dbRes);
       res.status(200).json(dbRes);
     })
     .catch(next);
 });
+
+// router.get("/trips/:id", (req, res, next) => {
+//   Day.findById(req.params.id)
+//     .populate("activities")
+//     .then((dbRes) => {
+//       console.log("this is back:", dbRes);
+//       res.status(200).json(dbRes);
+//     })
+//     .catch(next);
+// });
 
 module.exports = router;
