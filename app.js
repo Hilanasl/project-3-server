d939c946448d70c6756382611afea0a9c347a510
Alix Virot <flying.cat@mac.com>
2022-02-09 16:41:13 +0100

    test merge2

--- app.js
+++ app.js
@@ -8,15 +8,15 @@ const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+const tripsRouter = require("./routes/trips");
 //const authRouter = require("./routes/auth");
-
 
 const app = express();
 
 app.use(
   cors({
     origin: "http://localhost:3000",
-    credentials: true
+    credentials: true,
   })
 );
 
@@ -27,9 +27,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+// app.get("/", (req, res) => res.send("server is running"));
+
+// app.use("/api/index", indexRouter);  <= ALIX : What I would do
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/trips", tripsRouter);
+
+// app.use("/trips", require('./routes/trips'))
 //app.use("/", authRouter);
 
+// ALIX ideas :
+// app.use("/api", indexRouter);
+// app.use("/api", require("./routes/index"))
+// app.use("/api/trips", require(indexRouter))
+// app.use("/api/trips", require(tripRouter)) and create a trip router
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
-var express = require('express');
+var express = require("express");
+const { get } = require("mongoose");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
 module.exports = router;
