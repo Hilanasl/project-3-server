9a03b5e2e0fda4a2a69cfb0fa65fdefc025cba87
Anna Baldwin <annabaldwin56@gmail.com>
2022-02-10 18:50:00 +0100

    thursday changes

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const User = require("./../models/UserModel");
-const Trip = require("./../models/TripModel");
+const UserModel = require("./../models/UserModel");
+const TripModel = require("./../models/TripModel");
 const isAuthenticated = require('../middlewares/jwt.middleware');
+const DayModel = require('../models/DayModel');
 
 
 /* GET users listing. */
@@ -13,15 +14,15 @@ router.get('/', function(req, res, next) {
 
 router.patch('/users/favourites/:id', isAuthenticated, async (req, res, next) => {
   try {
-    const user = await User.findOne({$and :[{id: req.payload._id}, { favourites: { $in: [req.params.id]}}]})
+    const user = await UserModel.findOne({$and :[{id: req.payload._id}, { favourites: { $in: [req.params.id]}}]})
     if (user) {
-      const updatedUser = await User.findByIdAndUpdate(req.payload._id, {
+      const updatedUser = await UserModel.findByIdAndUpdate(req.payload._id, {
         $pull: {favourites: req.params.id}
       }, {new: true})
       res.status(200).json(updatedUser);
     }
   else {
-    const updatedUser = await User.findByIdAndUpdate(req.payload._id, {
+    const updatedUser = await UserModel.findByIdAndUpdate(req.payload._id, {
       $addToSet: {favourites: req.params.id}
   }, {new: true} )
       res.status(200).json(updatedUser);
@@ -30,6 +31,20 @@ router.patch('/users/favourites/:id', isAuthenticated, async (req, res, next) =>
   next(err)
 }}
 )
+
+router.get('/users/profile', isAuthenticated, async (req, res, next) => {
+  try {
+    const userWithFaves = await UserModel.findById(req.payload._id)
+    .populate({
+      path:'favourites',
+      model: TripModel,
+      populate: { path: "days", model: DayModel },
+    })
+    res.status(200).json(userWithFaves)
+  } catch (err) {
+    next(err)
+  }
+})
 
 
 
